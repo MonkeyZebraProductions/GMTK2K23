61dda1b0fb17a32b1ba29e2314a70590d2359c44
MonkeyZebraProductions <73033054+MonkeyZebraProductions@users.noreply.github.com>
2023-07-09 15:36:09 +0100

    Speech Bubble

--- Anti Papers Please/Assets/Scripts/Enda Scripts/Person.cs
+++ Anti Papers Please/Assets/Scripts/Enda Scripts/Person.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Person : MonoBehaviour
 {
-    
+    public TMP_Text Speech;
+    public Canvas SpeechBubble;
 
     public void Speak(string dialogue)
     {
-
+        SpeechBubble.enabled = true;
+        Speech.text = dialogue;
     }
 }
